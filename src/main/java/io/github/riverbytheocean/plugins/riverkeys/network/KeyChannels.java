4be008c1de2c5124fb2e3cac54c4b499e7ee5216
riverbytheocean <refractionisaneatconcept@gmail.com>
2024-07-21 12:50:23 -0400

    change the channels to listen to
    the mod isn't going to be compatible with AriKeys, so might as well switch the channel

--- src/main/java/io/github/riverbytheocean/plugins/riverkeys/network/KeyChannels.java
+++ src/main/java/io/github/riverbytheocean/plugins/riverkeys/network/KeyChannels.java
@@ -2,9 +2,9 @@ package io.github.riverbytheocean.plugins.riverkeys.network;
 
 public class KeyChannels {
 
-    public static final String HANDSHAKE = "arikeys:greeting";
-    public static final String ADD_KEY = "arikeys:addkey";
-    public static final String KEY_PRESS = "arikeys:keybind";
-    public static final String LOAD_KEYS = "arikeys:load";
+    public static final String HANDSHAKE = "riverkeys:greeting";
+    public static final String ADD_KEY = "riverkeys:addkey";
+    public static final String KEY_PRESS = "riverkeys:keybind";
+    public static final String LOAD_KEYS = "riverkeys:load";
 
 }
